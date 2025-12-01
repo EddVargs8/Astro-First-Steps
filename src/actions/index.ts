70f0949da0c95e0d0fcce0e55d61e6b7568483e2
EddVargs8 <l19290961@tectijuana.edu.mx>
2025-11-30 17:32:42 -0800

    asctro actions with node adapter

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,14 +1,29 @@
 import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
+const users = [
+  { email: "user@example.com", password: "password123" },
+  { email: "admin@example.com", password: "adminpass" },
+  { email: "guest@example.com", password: "guestpass" },
+];
+
 export const server = {
-  ping: defineAction({
+  login: defineAction({
+    accept: "form",
     input: z.object({
-      name: z.string(),
+      email: z.string().email(),
+      password: z.string().min(6),
     }),
-    handler: async ({ name }) => {
-      console.log("Action ejecutada. Nombre:", name);
-      return { message: `Hola ${name}!` };
+    handler: async (input) => {
+      const user = users.find(
+        (u) => u.email === input.email && u.password === input.password,
+      );
+
+      if (user) {
+        console.log("Login successful!");
+      } else {
+        console.log("Invalid email or password.");
+      }
     },
   }),
 };
